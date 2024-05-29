b6e0d953c88aff0ccc3c50d255912e1dcfa1bf26
telmanwisley <telmanwisley@gmail.com>
2024-05-29 21:07:01 +0200

    feat:add distribute point

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import CheckPoints from "./components/CheckPoints";
+import DistributePoint from "./components/DistributePoint";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-between p-24">
       <CheckPoints />
+      <DistributePoint />
     </main>
   );
 }
